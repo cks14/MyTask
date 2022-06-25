51524e02f62554149c0e39facd02a4d33d07403d
Satyendra <cks.14sonu@gmail.com>
2022-06-25 21:41:30 +0530

    new empty activity added

---
+++ app/src/main/java/com/mytask/webservices/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mytask.webservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mytask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
